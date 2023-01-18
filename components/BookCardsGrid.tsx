108f2139349cc55ea39f2b0033e8ce658f9112c5
LBeul <l.beul@outlook.de>
2023-01-18 14:52:11 +0100

    remove api and use local object to reduce latency

--- components/BookCardsGrid.tsx
+++ components/BookCardsGrid.tsx
@@ -2,27 +2,12 @@ import { SimpleGrid, Skeleton } from '@chakra-ui/react';
 import { BookCardsGridProps } from '../interfaces';
 import BookCard from './BookCard';
 
-const BookCardsGrid = ({ bookList, isLoaded }: BookCardsGridProps) => {
-  return isLoaded ? (
+const BookCardsGrid = ({ bookList }: BookCardsGridProps) => {
+  return (
     <SimpleGrid minChildWidth='275px' spacing='10px' w='100%'>
       {bookList?.map((book) => (
         <BookCard book={book} key={book.isbn} />
       ))}
-    </SimpleGrid>
-  ) : (
-    <SkeletonGrid />
-  );
-};
-
-const SkeletonGrid = (): JSX.Element => {
-  return (
-    <SimpleGrid minChildWidth='275px' spacing='10px' w='100%'>
-      <Skeleton height='150px' />
-      <Skeleton height='150px' />
-      <Skeleton height='150px' />
-      <Skeleton height='150px' />
-      <Skeleton height='150px' />
-      <Skeleton height='150px' />
     </SimpleGrid>
   );
 };
--- pages/books.tsx
+++ pages/books.tsx
@@ -1,22 +1,54 @@
 import { NextPage } from 'next';
-import { useEffect, useState } from 'react';
 import { Book } from '../interfaces';
 import BookCardsGrid from '../components/BookCardsGrid';
 
 const Books: NextPage = () => {
-  const [isLoaded, setIsLoaded] = useState<boolean>(false);
-  const [bookList, setBookList] = useState<Book[] | null>(null);
-  useEffect(() => fetchBooks('http://localhost:3000/api/books'), []);
-
-  const fetchBooks = (url: string): void => {
-    setIsLoaded(false);
-    fetch(url)
-      .then((response) => response.json())
-      .then((data) => setBookList(data));
-    setIsLoaded(true);
-  };
-
-  return <BookCardsGrid isLoaded={isLoaded} bookList={bookList} />;
+  return <BookCardsGrid bookList={bookList} />;
 };
+
+const bookList: Book[] = [
+  {
+    isbn: '9780593135204',
+    title: 'Project Hail Mary',
+    author: 'Andy Weir',
+    review: 'Emphasizes the "Sci" in SciFi',
+    tags: ['sci-fi', 'fiction'],
+  },
+  {
+    isbn: '9780553418026',
+    title: 'The Martian',
+    author: 'Andy Weir',
+    review: 'Better than the movie',
+    tags: ['sci-fi', 'fiction'],
+  },
+  {
+    isbn: '9781407191188',
+    title: 'Northern Lights',
+    author: 'Philip Pullman',
+    review: 'Underestimated masterpiece',
+    tags: ['fantasy', 'fiction'],
+  },
+  {
+    isbn: '9780547517650',
+    title: 'The Joy of X',
+    author: 'Steven Strogatz',
+    review: 'Complex math explained',
+    tags: ['non-fiction', 'science'],
+  },
+  {
+    isbn: '9781449389550',
+    title: 'Hackers & Painters',
+    author: 'Paul Graham',
+    review: "Essays of YC's founder",
+    tags: ['non-fiction', 'essays'],
+  },
+  {
+    isbn: '9781665190565',
+    title: 'The Furthest Station',
+    author: 'Ben Aaronovitch',
+    review: 'Policing meets Magic',
+    tags: ['fiction', 'fantasy'],
+  },
+];
 
 export default Books;
